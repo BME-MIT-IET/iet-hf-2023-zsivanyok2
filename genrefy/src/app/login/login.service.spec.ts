78653aa6bf12e0c86230a35787e19ff358e55e6e
levente-murgas <66883416+levente-murgas@users.noreply.github.com>
2023-05-18 21:31:17 +0200

    #5 LoginService getAccessToken test
    Ez a teszt a LoginService getAccessToken metódusát vizsgálja. Az getAccessToken metódus feladata, hogy lekérje az hozzáférési tokent a Spotify API-tól.
    
    Először létrehozzuk a tesztelendő paramétereket (clientId és code), valamint a mockolt hozzáférési tokent és választ, amit a fetch függvény hívásakor várunk.
    
    Mockoljuk a window.fetch függvényt, hogy a teszt során az valóban csak a mockolt választ adjon vissza.
    
    Mockoljuk a localStorage.getItem függvényt is, hogy a teszt során a 'verifier' kulcshoz tartozó érték is definiált legyen.
    
    Ezután meghívjuk a getAccessToken metódust a megadott paraméterekkel, és megvárjuk, míg befejeződik (async-await segítségével).
    
    A teszt végén ellenőrizzük, hogy a fetch függvény a várt paraméterekkel lett-e meghívva, és hogy a getAccessToken metódus a várt hozzáférési tokent adja-e vissza.

--- genrefy/src/app/login/login.service.spec.ts
+++ genrefy/src/app/login/login.service.spec.ts
@@ -4,6 +4,7 @@ import { LoginService } from './login.service';
 
 describe('LoginService', () => {
   let service: LoginService;
+  let fetchSpy: jasmine.Spy;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -29,4 +30,32 @@ describe('LoginService', () => {
     });
     expect(profile).toEqual(mockResponse);
   });
+
+  it('#getAccessToken should fetch access token', async () => {
+    const clientId = 'testClientId';
+    const code = 'testCode';
+    const mockAccessToken = 'testAccessToken';
+    const mockResponse = new Response(JSON.stringify({ access_token: mockAccessToken }));
+    const mockVerifier = 'testVerifier';
+  
+    spyOn(window, 'fetch').and.returnValue(Promise.resolve(mockResponse));
+    spyOn(localStorage, 'getItem').and.returnValue(mockVerifier);
+  
+    const accessToken = await service.getAccessToken(clientId, code);
+    
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("redirect_uri", "http://localhost:4200/login");
+    params.append("code_verifier", mockVerifier);
+  
+    expect(window.fetch).toHaveBeenCalledWith('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: params,
+    });
+    expect(accessToken).toEqual(mockAccessToken);
+  });
+
 });
