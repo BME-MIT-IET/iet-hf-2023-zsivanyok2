61481a74fd46e7373c6bd7774c7d22f7e94665bf
levente-murgas <66883416+levente-murgas@users.noreply.github.com>
2023-05-18 21:14:19 +0200

    #5 LoginService fetchProfile test
    Ebben a tesztben az LoginService szolgáltatás fetchProfile metódusát teszteljük, amely a Spotify API-t használva lekéri a felhasználó profilját. A teszt lépései a következők:
    
    Létrehozunk egy hamis választ (mockResponse), amelyet a fetch hívás során vissza szeretnénk adni, továbbá egy hamis token-t (mockToken), amit a metódus paramétereként fogunk átadni.
    
    Beállítjuk a fetch hívást, hogy a hamis választ adjon vissza a mockToken használatával.
    
    Meghívjuk a fetchProfile metódust a mockToken-nel, és elvárjuk, hogy a visszaadott profil megegyezzen a hamis válasszal.
    
    Ellenőrizzük, hogy a fetch hívás a megfelelő paraméterekkel történt-e, beleértve a token-t a fejlécben.
    
    A teszt célja, hogy biztosítsa a fetchProfile metódus helyes működését, valamint hogy a fetch hívás a megfelelő paraméterekkel történik.

--- genrefy/src/app/login/login.service.spec.ts
+++ genrefy/src/app/login/login.service.spec.ts
@@ -13,4 +13,20 @@ describe('LoginService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should fetch profile with correct token', async () => {
+    const mockResponse = { name: 'test' };
+    const mockToken = 'mock-token';
+    const fetchResponse = new Response(JSON.stringify(mockResponse));
+
+    spyOn(window, 'fetch').and.returnValue(Promise.resolve(fetchResponse));
+
+    const profile = await service.fetchProfile(mockToken);
+
+    expect(window.fetch).toHaveBeenCalledWith('https://api.spotify.com/v1/me', {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${mockToken}` },
+    });
+    expect(profile).toEqual(mockResponse);
+  });
 });
