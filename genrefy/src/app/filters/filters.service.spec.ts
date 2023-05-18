322bdf97a7f4744009666a1a66a04f2ddff23dfa
levente-murgas <66883416+levente-murgas@users.noreply.github.com>
2023-05-18 20:34:35 +0200

    #5 FiltersService should create
    Added mock dependencies to test FiltersService initialization.

--- genrefy/src/app/filters/filters.service.spec.ts
+++ genrefy/src/app/filters/filters.service.spec.ts
@@ -1,16 +1,30 @@
 import { TestBed } from '@angular/core/testing';
-
 import { FiltersService } from './filters.service';
+import { SongsService } from '../songs/songs.service';
+import { of } from 'rxjs';
 
 describe('FiltersService', () => {
   let service: FiltersService;
+  let mockSongsService = jasmine.createSpyObj('SongsService', ['getLikedTracksFromLocalStorage']);
+
+  let localStorageSpy = jasmine.createSpyObj('localStorage', ['getItem', 'setItem']);
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: SongsService, useValue: mockSongsService },
+        { provide: 'localStorage', useValue: localStorageSpy },
+        FiltersService
+      ]
+    });
     service = TestBed.inject(FiltersService);
+    mockSongsService.getLikedTracksFromLocalStorage.and.returnValue([]);
+    localStorageSpy.getItem.and.returnValue(null);
   });
 
-  it('should be created', () => {
+  it('should create', () => {
     expect(service).toBeTruthy();
   });
+
+  // Additional tests...
 });
