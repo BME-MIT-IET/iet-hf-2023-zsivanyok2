c871ce1b2839bdcb71332917039a2109c094f264
levente-murgas <66883416+levente-murgas@users.noreply.github.com>
2023-05-18 19:19:59 +0200

    #1 1. LoginComponent: should create

--- genrefy/src/app/app.component.spec.ts
+++ genrefy/src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('genrefy');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('genrefy app is running!');
-  });
 });
--- genrefy/src/app/login/login.component.spec.ts
+++ genrefy/src/app/login/login.component.spec.ts
@@ -1,14 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
 
 import { LoginComponent } from './login.component';
+import { LoginService } from './login.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  const mockActivatedRoute = {
+    queryParams: of({ code: 'mockCode' })
+  };
+  
+  const mockRouter = {
+    navigate: jasmine.createSpy('navigate') // Spy on the navigate method.
+  };
+  
+  const mockLoginService = {
+    redirectToAuthCodeFlow: jasmine.createSpy('redirectToAuthCodeFlow'), // Spy on redirectToAuthCodeFlow method.
+    getAccessToken: jasmine.createSpy('getAccessToken').and.returnValue(Promise.resolve('mockAccessToken')), // Spy on getAccessToken method.
+    fetchProfile: jasmine.createSpy('fetchProfile').and.returnValue(Promise.resolve({ id: 'mockUserId' })) // Spy on fetchProfile method.
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: Router, useValue: mockRouter },
+        { provide: LoginService, useValue: mockLoginService }
+      ]
     })
     .compileComponents();
 
@@ -20,4 +42,6 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
 });
