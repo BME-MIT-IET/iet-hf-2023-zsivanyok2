12c83eaf77dd7ba0145e6962cf1107c6d8b3b60e
levente-murgas <66883416+levente-murgas@users.noreply.github.com>
2023-05-18 20:30:38 +0200

    #5 FiltersComponent should create
    Added mock dependencies to test FiltersComponent initialization.

--- genrefy/src/app/filters/filters.component.spec.ts
+++ genrefy/src/app/filters/filters.component.spec.ts
@@ -1,23 +1,53 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { FiltersComponent } from './filters.component';
+import { FiltersService } from './filters.service';
+import { of } from 'rxjs';
+import { MatCardModule } from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
 
 describe('FiltersComponent', () => {
   let component: FiltersComponent;
   let fixture: ComponentFixture<FiltersComponent>;
 
+  let mockFiltersService = jasmine.createSpyObj('FiltersService', ['fetchLikedTracksWithGenres', 'getTopGenres', 'getFilteredSongs', 'getSelectedGenres', 'filterSongsByGenres', 'setFilters']);
+  let mockRouter = jasmine.createSpyObj('Router', ['navigate']);
+
+  let localStorageSpy = jasmine.createSpyObj('localStorage', ['getItem']);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FiltersComponent ]
+      imports: [RouterTestingModule,
+         MatCardModule,
+         MatChipsModule],
+      declarations: [ FiltersComponent ],
+      providers: [
+        { provide: FiltersService, useValue: mockFiltersService },
+        { provide: 'localStorage', useValue: localStorageSpy }
+      ]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(FiltersComponent);
     component = fixture.componentInstance;
+
+    mockFiltersService.fetchLikedTracksWithGenres.and.returnValue(Promise.resolve([]));
+    mockFiltersService.getTopGenres.and.returnValue([]);
+    mockFiltersService.getFilteredSongs.and.returnValue([]);
+    mockFiltersService.getSelectedGenres.and.returnValue(new Set<string>());
+    mockFiltersService.filterSongsByGenres.and.returnValue([]);
+
+    localStorageSpy.getItem.and.returnValue(null);
+    
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  // Additional tests...
 });
