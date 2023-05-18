5d4e8fbb056d3b5f6121adede6e9952d7925675a
levente-murgas <66883416+levente-murgas@users.noreply.github.com>
2023-05-18 20:27:07 +0200

    #5 SongsService should be created
    Added mock dependencies to check the initialization of SongsService

--- genrefy/src/app/songs/songs.service.spec.ts
+++ genrefy/src/app/songs/songs.service.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { SongsService } from './songs.service';
 
 describe('SongsService', () => {
   let service: SongsService;
+  let httpTestingController: HttpTestingController;
+
+  // Mock localStorage
+  let localStorageSpy: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    localStorageSpy = jasmine.createSpyObj('localStorage', ['getItem', 'setItem']);
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        SongsService,
+        { provide: 'localStorage', useValue: localStorageSpy }
+      ]
+    });
+
     service = TestBed.inject(SongsService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify(); // Verify that no unmatched requests are outstanding
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  // Other tests go here...
 });
